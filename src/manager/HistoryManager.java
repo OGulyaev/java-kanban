cbea6b7df4479a9078d03a0a341b501fb3303a1b
Oleg Gulyaev <kwwlogin@yandex.ru>
2025-03-20 20:00:29 +0300

    Change name metohod addInHistory

--- src/manager/HistoryManager.java
+++ src/manager/HistoryManager.java
@@ -6,6 +6,6 @@ import java.util.ArrayList;
 
 public interface HistoryManager {
     ArrayList<Task> getHistory();
-
+// заменить название метода addInHostory на add
     void addInHistory(Task task);
 }
